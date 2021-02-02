5f0543666c247eda5b6632e990d34ff057a137a3
Brigham Andersen <brighamband@gmail.com>
2021-02-01 18:04:30 -0700

    Evil hangman basic I/O done

--- EvilHangman/src/hangman/EvilHangman.java
+++ EvilHangman/src/hangman/EvilHangman.java
@@ -1,19 +1,52 @@
 package hangman;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class EvilHangman {
 
     public static void main(String[] args) {
-
+        File dictionary = new File(args[0]);
+        int wordLength = Integer.parseInt(args[1]);
+        int guesses = Integer.parseInt(args[2]);
 
         IEvilHangmanGame evilHangmanGame = new EvilHangmanGame();
-//        evilHangmanGame.startGame();
 
         try {
-            evilHangmanGame.makeGuess('q');
-        } catch (GuessAlreadyMadeException ex) {
+            evilHangmanGame.startGame(dictionary, wordLength);
+        } catch (IOException | EmptyDictionaryException ex) {
             ex.printStackTrace();
         }
-        System.out.println(evilHangmanGame.getGuessedLetters());
+
+        while (guesses > 0) {
+
+            System.out.println("You have " + guesses + " guesses left");
+
+            System.out.print("Used letters: ");
+            System.out.println(evilHangmanGame.getGuessedLetters());
+
+            System.out.print("Word: ");
+            System.out.println(); // ?????????????
+
+            System.out.print("Enter a guess: ");
+            Scanner scanner = new Scanner(System.in);
+            char guess = scanner.next().charAt(0);
+
+            System.out.println("You guessed " + guess);
+
+
+            try {
+                evilHangmanGame.makeGuess(guess);
+            } catch (GuessAlreadyMadeException ex) {
+                ex.printStackTrace();
+            }
+            guesses--;
+        }
+        // FIXME - Add logic in case they win
+        System.out.println("You lose!");
+        // FIXME - Add new correct word
+        System.out.println("The word was: FIXME");
     }
 
 }
--- EvilHangman/src/hangman/EvilHangmanGame.java
+++ EvilHangman/src/hangman/EvilHangmanGame.java
@@ -2,28 +2,59 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
     SortedSet<Character> guessedLetters;
+    Set<String> hangmanDictionary;
 
     public EvilHangmanGame() {
         guessedLetters = new TreeSet<Character>();
+        hangmanDictionary = new HashSet<String>();
     }
 
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+        if (dictionary == null) {
+            throw new IOException("Dictionary does not exist.");
+        }
 
+        if (dictionary.length() == 0) {
+            throw new EmptyDictionaryException("Dictionary does not contain any words.");
+        }
+
+        Scanner scanner = new Scanner(dictionary);
+        String curWord;
+
+        while (scanner.hasNext()) {
+            curWord = scanner.next().toLowerCase();
+
+            if(curWord.length() == wordLength) {
+                hangmanDictionary.add(curWord);
+            }
+        }
+        scanner.close();
+
+        if (hangmanDictionary.size() == 0) {
+            throw new EmptyDictionaryException("No words in dictionary are that length.");
+        }
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
 
+        if (guessedLetters.contains(guess)) {
+            throw new GuessAlreadyMadeException("You already guessed that letter.");
+        }
+
         guessedLetters.add(guess);
-        guessedLetters.add('a');
-        guessedLetters.add('z');
+
+        // If exists
+        System.out.println("Yes, there is {frequency} " + guess);
+//        else {
+            System.out.println("Sorry, there are no " + guess + "\'s");
+//        }
+        System.out.println();
 
         return null;
     }
