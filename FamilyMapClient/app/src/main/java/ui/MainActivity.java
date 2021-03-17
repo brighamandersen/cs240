f4bd7e5168e17ada2bd2280630908eafc213b763
Brigham Andersen <brighamband@gmail.com>
2021-03-16 23:54:16 -0600

    Added new files

---
+++ FamilyMapClient/app/src/main/java/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cs240.familymapclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
